e30ec8f4176820acd41ae680d87a243ebde4a25e
Mariano Lara <mariano.lara.ga@gmail.com>
2025-10-27 13:31:50 +0100

    Primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  return <div>
+    <h1>Hello World</h1>
+    <p>Count: {count}</p>
+    <button onClick={() => setCount(count + 1)}>Increment</button>
+  </div>;
+}
