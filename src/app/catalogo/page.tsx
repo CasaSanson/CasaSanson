87ed02757861584badb0fc0fce967e230d960177
CasaSanson <admin@casasanson.com>
2025-12-15 17:45:07 +0100

    Fix folder case to lowercase for Vercel

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import LastRelease from "@/components/shop/LastRelease";
 import Store from "@/components/shop/Store";
-import Link from "next/link";
 
 export default function Catalogo() {
     return (
