a29262dfe1da49156b682dd248027db9d3a651cc
CasaSanson <admin@casasanson.com>
2025-12-15 16:30:52 +0100

    trial

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [],
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
   },
 }
 
