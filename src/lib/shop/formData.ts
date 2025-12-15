f2cb4a4502392a6f7b3d242f52afb4fdf37ed8ff
CasaSanson <admin@casasanson.com>
2025-12-15 18:08:31 +0100

    Add lib/shop/formData.ts to fix casing issue

---
+++ src/lib/shop/formData.ts
@@ -0,0 +1,13 @@
+export interface FormData {
+    nombre: string;
+    apellido: string;
+    email: string;
+    telefono: string;
+    direccion: string;
+    ciudad: string;
+    codigoPostal: string;
+    pais: string;
+    metodoEnvio: string;
+    talla: string;
+    personalizado: string;
+}
