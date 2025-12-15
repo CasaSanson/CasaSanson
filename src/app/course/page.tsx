ebb950b776ad9aee019b975bd7568ff66b5ac0e9
CasaSanson <admin@casasanson.com>
2025-12-15 16:21:20 +0100

    Remove Course

--- src/app/course/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Flipbook from "@/components/Flipbook";
-export default function Course() {
-    return (
-     <main className="bg-cloth_black min-h-screen w-[100%] mx-auto mt-20">
-        <Flipbook/>
-    </main>
-    );
-}
